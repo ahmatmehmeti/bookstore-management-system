90840ad0205b58ca921d362edb804c640f546572
ahmat <ahmat.mehmeti97@gmail.com>
2021-04-07 00:16:01 +0200

    fixes

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -23,28 +23,28 @@
                     @if(auth()->user()->isAdmin())
                         <li class="nav-item">
                             <a
-                                class="nav-link {{ Route::currentRouteNamed(['users.index','users.create']) ? 'active' : '' }}"
+                                class="nav-link {{ Route::currentRouteNamed(['users.index','users.create','users.edit','users.update','users.store','users.destroy']) ? 'active' : '' }}"
                                 href="{{ route('users.index')}}">
                                 <i class="nav-icon fas fa-users"></i>&nbsp;<p class="text">Users</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a
-                                class="nav-link "
+                                class="nav-link {{ Route::currentRouteNamed('postiers.index') ? 'active' : '' }}"
                                 href="{{ route('postiers.index') }}">
                                 <i class="nav-icon fas fa-people-carry"></i>&nbsp;<p class="text">Postiers</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a
-                                class="nav-link"
+                                class="nav-link {{ Route::currentRouteNamed(['categories.index','categories.create','categories.edit','categories.update','categories.store','categories.destroy']) ? 'active' : '' }}"
                                 href="{{ route('categories.index') }}">
                                 <i class="nav-icon fas fa-edit"></i>&nbsp;<p class="text">Categories</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a
-                                class="nav-link"
+                                class="nav-link {{ Route::currentRouteNamed(['books.index','books.create','books.edit','books.update','books.store','books.destroy']) ? 'active' : '' }}"
                                 href="{{ auth()->user()->isAdmin() ? route('books.index') : route('orders.create') }}">
                                 <i class="nav-icon fas fa-book"></i>&nbsp;<p class="text">Books</p>
                             </a>
@@ -53,7 +53,7 @@
 
                     <li class="nav-item">
                         <a
-                            class="nav-link"
+                            class="nav-link {{ Route::currentRouteNamed(['orders.index','orders.create','orders.edit','orders.update','orders.store','orders.destroy']) ? 'active' : '' }}"
                             href="{{ route('orders.index') }}">
                             <i class="nav-icon fas fa-edit"></i>&nbsp;<p class="text">Orders</p>
                         </a>
@@ -61,14 +61,14 @@
                         <br>
                         <li class="nav-item">
                             <a
-                                class="nav-link"
+                                class="nav-link {{ Route::currentRouteNamed(['home']) ? 'active' : '' }}"
                                 href="{{ route('home') }}">
                                 <i class="nav-icon fas fa-poll-h"></i>&nbsp;<p class="text">Dashboard</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a
-                                class="nav-link"
+                                class="nav-link  {{ Route::currentRouteNamed(['profile','profile.update','profile.updatePassword']) ? 'active' : '' }}"
                                 href="{{ route('profile') }}">
                                 <i class="nav-icon fas fa-user"></i>&nbsp;<p class="text">MyProfile</p>
                             </a>
